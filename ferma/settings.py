f2acd5a2afcdd158ad14b288a351831c034f53f3
Adebola Adeola <alkadelik@Adebolas-MacBook-Pro.local>
2023-04-05 18:01:40 +0100

    updated settings for pythonanywhere

--- ferma/settings.py
+++ ferma/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 CORS_ALLOW_ALL_ORIGINS: True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8080',
+    'https://alkadelik-github-io.vercel.app',
+    'https://alkadelik-github-io-git-main-alkadelik.vercel.app',
 ]
 
 MIDDLEWARE = [
