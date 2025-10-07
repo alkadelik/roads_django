de419b2a16323879895587eb1ed0235456b32e3a
alkadelik <debola_adeola@yahoo.com>
2025-10-07 18:29:15 +0100

    updated allowed host

--- ferma/settings.py
+++ ferma/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['165.232.43.76',]
 
 
 # Application definition
